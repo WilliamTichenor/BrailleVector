72e4762dac706c010a4b1115a7d0a856d8e4a877
WilliamTichenor <wctich@gmail.com>
2024-11-12 12:23:36 -0600

    trivial method addition

--- utils.py
+++ utils.py
@@ -5,5 +5,8 @@ import pybrl as brl
 def textToBraille(str):
     return brl.toUnicodeSymbols(brl.translate(str), flatten=True)
 
+def textToSVG(str, mirror=False):
+    pass
+
 if __name__ == "__main__":
     print(brl.toUnicodeSymbols(brl.translate(input("Enter: ")), flatten=True))
